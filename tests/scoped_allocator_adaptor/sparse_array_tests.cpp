a4d5399e8964a652f752bb75fc3521c38ba57586
Lukas Kerkemeier <lukas.kerkemeier@web.de>
2021-05-27 13:00:08 +0200

    Now scoped allocators can be used. However emplace still does not work as it should.

--- tests/scoped_allocator_adaptor/sparse_array_tests.cpp
+++ tests/scoped_allocator_adaptor/sparse_array_tests.cpp
@@ -3,8 +3,8 @@
 #include <memory>
 #include <scoped_allocator>
 #include <tsl/sparse_set.h>
-#include <unordered_set>
 #include <type_traits>
+#include <unordered_set>
 
 // Globals
 constexpr auto MAX_INDEX = 32; // BITMAP_NB_BITS
@@ -34,21 +34,59 @@ void set(std::initializer_list<typename T::value_type> l) {
 }
 
 template <typename T> void uses_allocator() {
-  BOOST_TEST_REQUIRE((std::uses_allocator<typename T::Array, typename T::Allocator>::value),
-                     "uses_allocator returns false");
+  BOOST_TEST_REQUIRE(
+      (std::uses_allocator<typename T::Array, typename T::Allocator>::value),
+      "uses_allocator returns false");
 }
 
-template <typename T, typename ...Args> void trailing_allocator_convention(Args ...) {
+template <typename T, typename... Args>
+void trailing_allocator_convention(Args...) {
   using Alloc = typename T::Allocator;
-  BOOST_TEST_REQUIRE((std::is_constructible<typename T::Array, Args..., const Alloc&>::value),
-                     "trailing_allocator thinks construction is not possible");
+  BOOST_TEST_REQUIRE(
+      (std::is_constructible<typename T::Array, Args..., const Alloc &>::value),
+      "trailing_allocator thinks construction is not possible");
 }
 
-template <typename T> void is_constructable_test() {
-  auto alloc = typename std::allocator_traits<typename T::Allocator>::rebind_alloc<
-      typename T::Array>();
-  auto const& alloc_ref = alloc;
-  std::vector<typename T::Array, typename T::Allocator> vector(alloc_ref);
+template <typename T> void trailing_allocator_convention_without_parameters() {
+  using Alloc = typename std::allocator_traits<
+      typename T::Allocator>::template rebind_alloc<typename T::Array>;
+  BOOST_TEST_REQUIRE(
+      (std::is_constructible<typename T::Array, const Alloc &>::value),
+      "trailing_allocator thinks construction is not possible");
+}
+
+/** This test creates a memory leak.
+ * However it is only one single sparse_array and with this, the test is
+ * simpler.
+ */
+template <typename T>
+void is_move_insertable(std::initializer_list<typename T::value_type> l) {
+  using A = typename std::allocator_traits<
+      typename T::Allocator>::template rebind_alloc<typename T::Array>;
+  A m;
+  auto p = m.allocate(1);
+  typename T::Allocator ArrayAlloc;
+  typename T::Array rv(MAX_INDEX, ArrayAlloc);
+  std::size_t counter = 0;
+  for (auto const &value : l) {
+    rv.set(ArrayAlloc, counter++, value);
+  }
+  std::cout << "Before\n";
+  std::allocator_traits<A>::construct(m, p, std::move(rv));
+  std::cout << "After\n";
+  rv.clear(ArrayAlloc);
+}
+
+/** This test creates a memory leak.
+ * However it is only one single sparse_array and with this, the test is
+ * simpler.
+ */
+template <typename T> void is_default_insertable() {
+  using A = typename std::allocator_traits<
+      typename T::Allocator>::template rebind_alloc<typename T::Array>;
+  A m;
+  typename T::Array *p = m.allocate(1);
+  std::allocator_traits<A>::construct(m, p);
 }
 
 template <typename T, tsl::sh::sparsity Sparsity = tsl::sh::sparsity::medium>
@@ -72,15 +110,29 @@ BOOST_AUTO_TEST_CASE(normal_compilation) { compilation<NORMAL<int>>(); }
 BOOST_AUTO_TEST_CASE(normal_construction) { construction<NORMAL<int>>(); }
 BOOST_AUTO_TEST_CASE(normal_set) { set<NORMAL<int>>({0, 1, 2, 3, 4}); }
 BOOST_AUTO_TEST_CASE(normal_uses_allocator) { uses_allocator<NORMAL<int>>(); }
-BOOST_AUTO_TEST_CASE(normal_leading_allocator_convention) {trailing_allocator_convention<NORMAL<int>>(0); }
-BOOST_AUTO_TEST_CASE(normal_is_constructable_test) {is_constructable_test<NORMAL<int>>(); }
+BOOST_AUTO_TEST_CASE(normal_trailing_allocator_convention) {
+  trailing_allocator_convention<NORMAL<int>>(0);
+}
+BOOST_AUTO_TEST_CASE(normal_is_move_insertable) {
+  is_move_insertable<NORMAL<int>>({0, 1, 2, 3, 4, 5});
+}
+BOOST_AUTO_TEST_CASE(normal_is_default_insertable) {
+  is_default_insertable<NORMAL<int>>();
+}
 
 BOOST_AUTO_TEST_CASE(scoped_compilation) { compilation<SCOPED<int>>(); }
 BOOST_AUTO_TEST_CASE(scoped_construction) { construction<SCOPED<int>>(); }
 BOOST_AUTO_TEST_CASE(scoped_set) { set<SCOPED<int>>({0, 1, 2, 3, 4}); }
 BOOST_AUTO_TEST_CASE(scoped_uses_allocator) { uses_allocator<SCOPED<int>>(); }
-BOOST_AUTO_TEST_CASE(scoped_leading_allocator_convention) {trailing_allocator_convention<SCOPED<int>>(0); }
-BOOST_AUTO_TEST_CASE(scoped_is_constructable_test) {is_constructable_test<SCOPED<int>>(); }
+BOOST_AUTO_TEST_CASE(scoped_trailing_allocator_convention) {
+  trailing_allocator_convention<SCOPED<int>>(0);
+}
+BOOST_AUTO_TEST_CASE(scoped_is_move_insertable) {
+  is_move_insertable<SCOPED<int>>({0, 1, 2, 3, 4, 5});
+}
+BOOST_AUTO_TEST_CASE(scoped_is_default_insertable) {
+  is_default_insertable<SCOPED<int>>();
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
